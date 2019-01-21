77ac50f42201ab993a64cf63dcb0bef59430b293
Luthfi Aji <luthfi.ajiw@gmail.com>
2019-01-21 17:10:22 +0700

    logic 1

--- logic1.js
+++ logic1.js
@@ -2,6 +2,7 @@ var a = [];
 
 //SOAL 1
 function soal1(n) {
+  console.log('SOAL 1');
   for (let y = 1; y <= n; y++) {
     a[y] = new Array(y);
     for (let x = 1; x <= n; x++) {
@@ -14,12 +15,14 @@ function soal1(n) {
   }
 
   console.log(a);
+  console.log('=============');
 }
 
 soal1(9);
 
 //SOAL 2
 function soal2(n) {
+  console.log('SOAL 2');
   for (let y = 1; y <= n; y++) {
     a[y] = new Array(y);
     for (let x = 1; x <= n; x++) {
@@ -31,12 +34,14 @@ function soal2(n) {
     }
   }
   console.log(a);
+  console.log('=============');
 }
 
 soal2(9);
 
 //SOAL 3
 function soal3(n) {
+  console.log('SOAL 3');
   for (let y = 1; y <= n; y++) {
     a[y] = new Array(y);
     for (let x = 1; x <= n; x++) {
@@ -49,12 +54,14 @@ function soal3(n) {
   }
 
   console.log(a);
+  console.log('=============');
 }
 
 soal3(9);
 
 //SOAL 4
 function soal4(n) {
+  console.log('SOAL 4');
   //check the n value
   if (n % 2 == 1) {
     n1 = Math.floor(n/2+1);
@@ -74,12 +81,15 @@ function soal4(n) {
   } else {
     console.log("Angka yang anda masukan bukan angka ganjil");
   }
+
+  console.log('=============');
 }
 
 soal4(9);
 
 //SOAL 5
 function soal5(n) {
+  console.log('SOAL 5');
   for (let y = 1; y <= n; y++) {
     a[y] = new Array(y);
     for (let x = 1; x <= n; x++) {
@@ -91,12 +101,14 @@ function soal5(n) {
     }
   }
   console.log(a);
+  console.log('=============');
 }
 
 soal5(9);
 
 //SOAL 6
 function soal6(n) {
+  console.log('SOAL 6');
   for (let y = 1; y <= n; y++) {
     for (let x = 1; x <= n; x++) {
       if (x >= n - (y-1)) {
@@ -107,6 +119,53 @@ function soal6(n) {
     }
   }
   console.log(a);
+  console.log('=============');
 }
 
 soal6(9);
+
+// SOAL 7
+function soal7(n) {
+  let a = []
+  console.log("SOAL 7");
+  for (let y = 0; y < n; y++) {
+    a[y] = new Array(y)
+    for (let x = 0; x < n; x++) {
+      if (x >= y && x <= n-(y+1)) {
+        a[y][x] = "*"
+      } else if (x <= y && x >= n-(y+1)) {
+        a[y][x] = "*"
+      } else {
+        a[y][x] = " "
+      }
+    }
+  }
+  console.log(a);
+
+  console.log("+++++++++++++");
+}
+
+soal7(9);
+
+// SOAL 8
+function soal8() {
+  console.log("SOAL 8");
+  let a = []
+  for (let y = 0; y < 9; y++) {
+    a[y] = new Array(y)
+    for (let x = 0; x < 9; x++) {
+      if (x <= y && x <= 9-(y+1)) {
+        a[y][x] = "*"
+      } else if (x >= y && x >= 9-(y+1)) {
+        a[y][x] = "*"
+      } else {
+        a[y][x] = " "
+      }
+    }
+  }
+
+  console.log(a);
+  console.log('+++++++++++++++');
+}
+
+soal8()
